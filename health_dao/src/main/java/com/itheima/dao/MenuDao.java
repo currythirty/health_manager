1c29a5354bcd9aa2f51a652bd6d6790f43cebfd2
15972953512 <lbg13142020>
2020-11-20 20:26:11 +0800

    李成洛-菜单页面的增删改查已全部完成，但有少许bug未解决，redis储存按照角色权限进行菜单显示已完成，也有一些bug需要完成，看到这里大家将整个项目重新拉取一下，以获得最新的项目代码。

---
+++ health_dao/src/main/java/com/itheima/dao/MenuDao.java
@@ -0,0 +1,47 @@
+package com.itheima.dao;
+
+import com.github.pagehelper.Page;
+import com.itheima.entity.QueryPageBean;
+import com.itheima.pojo.Menu;
+import com.itheima.pojo.Role;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+import java.util.Map;
+
+public interface MenuDao {
+
+    Page<Menu> findPage(QueryPageBean pageBean);
+
+    Page<Menu> queryPage(QueryPageBean pageBean);
+
+    void addMenu(Menu menu);
+
+    void addRoleMenu(Map map);
+
+    void editMenu(Map menu);
+
+    void deleteRoleMenu(Integer id);
+
+    void deleteMenu(Integer id);
+
+    @Select("select parentMenuId from t_menu where id = #{id}")
+    Integer isParentMenu(Integer id);
+
+    @Select("select count(*) from t_menu where parentMenuId = #{id}")
+    Integer countParentMenu(Integer id);
+
+    List<Map> findParentMenu();
+
+    List<Map> findAllRoles();
+
+    List<Integer> queryRolesChecked(Integer id);
+
+    @Select("select * from t_menu where id = #{id}")
+    Menu queryMenuById(Integer id);
+
+    @Select("select r.id id from t_role r,t_user_role ur,t_user u where u.username = #{username} and u.id = ur.user_id and ur.role_id = r.id")
+    Integer getRole(String username);
+
+    List<Map> getMenus(Integer id);
+}
